6ab7b8c2c3292fc5c0751a08b01a8fb51509d85b
Koshy George <kgeorge5@asu.edu>
2016-04-15 02:38:14 +0000

    Added debug message for reference store
    Signed-off-by: Koshy George <kgeorge5@asu.edu>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -71,7 +71,6 @@ import (
 	nwconfig "github.com/docker/libnetwork/config"
 	"github.com/docker/libtrust"
 	"golang.org/x/net/context"
-	
 )
 
 const (
@@ -1177,12 +1176,15 @@ func (daemon *Daemon) GetImageID(refOrID string) (image.ID, error) {
 	}
 	//Reload repositories.json and populate imageStore
 	jsonPath := "/var/lib/docker/image/aufs/repositories.json"
+
 	imageId, err := reference.GetNewStore(jsonPath, refOrID)
+	logrus.Debug("ImageID from reference store ", imageId)
 	if err != nil {
-	return "", ErrImageDoesNotExist{refOrID}
-	}else {
-	return imageId, nil
+		return "", ErrImageDoesNotExist{refOrID}
+	} else {
+		return imageId, nil
 	}
+
 	return "", ErrImageDoesNotExist{refOrID}
 }
 
